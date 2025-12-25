5195a919f184dd2e3403bede1b113477b668ae85
943mohamedzouaoui2131tg <97001589+943mohamedzouaoui2131tg@users.noreply.github.com>
2025-12-25 13:49:40 +0100

    cassandra_set_up_with_connection

--- cassandra_consumer/src/consumer.py
+++ cassandra_consumer/src/consumer.py
@@ -1,12 +1,21 @@
+import eventlet
+eventlet.monkey_patch()
+
+
 from kafka import KafkaConsumer
 import json
 from dotenv import load_dotenv
 import os;
+from cassandra.cluster import Cluster 
+
 
 
 load_dotenv()
 KAFKA_BROKER_ADDRESS1 = os.getenv("KAFKA_BROKER1")
 KAFKA_BROKER_ADDRESS2 = os.getenv("KAFKA_BROKER2")
+CASSANDRA_BROKER = os.getenv("CASSANDRA_BROKER")
+
+
 
 
 try:
@@ -20,6 +29,14 @@ try:
         )
 
     print("Listening for messages...")
+
+    print("Connected to Cassandra")
+    cluster = Cluster(["localhost"], port=3000)  # Connect to Cassandra on port 3000 in the local machine not docker containers
+    session = cluster.connect()
+    print("Connected to Cassandra cluster")
+
+    session.execute("USE Projet_bd;")
+
     try:
         for message in consumer:
             print(f"Received message: Topic: {message.topic}, Partition: {message.partition}, Offset: {message.offset}, Value: {message.value}")
